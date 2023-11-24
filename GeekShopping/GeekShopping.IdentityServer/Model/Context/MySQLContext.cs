afaf91424f3d8d6940f6fa8ecaa1cbb52edecf44
Romulo Lima <romulosilva-07@hotmail.com>
2023-11-24 00:20:34 -0300

    inserted nuget packages in IdentityServer

---
+++ GeekShopping/GeekShopping.IdentityServer/Model/Context/MySQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context
+{
+    public class DatabaseContext : IdentityDbContext<ApplicationUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    }
+}
